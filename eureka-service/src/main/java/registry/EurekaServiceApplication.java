9a2a15067c2a2df3062d4331a0929bfd14de24be
Luke Fox <l.fox9@nuigalway.ie>
2020-01-07 23:19:25 +0000

    adding profile bean to make eureka server aware it will be running on aws

--- eureka-service/src/main/java/registry/EurekaServiceApplication.java
+++ eureka-service/src/main/java/registry/EurekaServiceApplication.java
@@ -1,8 +1,13 @@
 package registry;
 
+import com.netflix.appinfo.AmazonInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @EnableEurekaServer
 @SpringBootApplication
@@ -10,5 +15,14 @@ public class EurekaServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaServiceApplication.class, args);
+    }
+
+    @Bean
+    @Profile("!default")
+    public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+        EurekaInstanceConfigBean b = new EurekaInstanceConfigBean(inetUtils);
+        AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+        b.setDataCenterInfo(info);
+        return b;
     }
 }
