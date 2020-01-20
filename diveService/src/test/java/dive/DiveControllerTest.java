16b7a7c4af421c1885be312dca5e80c4d7f88972
Luke Fox <l.fox9@nuigalway.ie>
2020-01-20 13:13:50 +0000

    removing mock test

--- diveService/src/test/java/dive/DiveControllerTest.java
+++ diveService/src/test/java/dive/DiveControllerTest.java
@@ -13,9 +13,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
 public class DiveControllerTest {
 
     @Test
