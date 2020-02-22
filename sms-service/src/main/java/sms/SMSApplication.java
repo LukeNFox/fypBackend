c0c9baf24c2663cc6cea8d2b905beb5b0e02fb6e
Luke Fox <l.fox9@nuigalway.ie>
2020-02-22 13:46:57 +0000

    initialise sms service and include it in gradle set up

---
+++ sms-service/src/main/java/sms/SMSApplication.java
@@ -0,0 +1,14 @@
+package sms;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SMSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SMSApplication.class, args);
+    }
+}
---
+++ sms-service/src/test/java/sms/SMSControllerTest.java
@@ -0,0 +1,21 @@
+package sms;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+public class SMSControllerTest {
+
+    @Test
+    public void sampleTest(){System.out.println("This should print");}
+
+}
